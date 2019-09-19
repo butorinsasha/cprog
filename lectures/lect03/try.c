509778a66e6058ff92a405223346e07537c5a6a9
Aleksandr Butorin <butorinsasha@gmail.com>
2019-09-19 21:21:42 +0300

    Structures tried

---
+++ lectures/lect03/try.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int arr[3] = {0, 3, 7};
+int len = sizeof(arr)/sizeof(arr[0]);
+
+typedef struct S {
+    char c; // 1 Bite
+    int i;  // 4 Bite
+} S;
+
+S s = {'A', 1};
+
+
+
+void main()
+{
+    for (int i = 0; i < len; ++i)
+       {
+            printf("%d\n", arr[i]);        
+       }
+
+    printf("Size of S: %ld\n", sizeof(S) ); // 8 // gcc -fpack-struct=11>> 5; // OR #pragma pack(push, 1) #pragma pack(pop)
+    printf("Size of int: %ld\n", sizeof(int) );
+    printf("s.c = %c\n", s.c);
+    printf("s.i = %d\n", s.i);
+
+}
---
+++ tests/test02/test02.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int i = 13;
+
+void main()
+{
+    printf("%d\n", i);        
+}
