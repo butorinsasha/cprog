4ec2383520409f6289fc89425f3a485ac5321f5a
Aleksandr Butorin <butorinsasha@gmail.com>
2019-09-26 21:27:37 +0300

    test02

--- tests/test02/test02.c
+++ tests/test02/test02.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 
-int i = 13;
+// int short arr[20];
+// short arr[10 << 1]
+// short int arr[10 * (10 % -3)];
+// arr[40] short;
+short arr[20];
+struct S {
+    char c; // 1 Bite
+    int i;  // 4 Bite
+};
+
+struct S s = {'A', 1};
+
 
 void main()
 {
-    printf("%d\n", i);        
+    printf("size of short arr[0] = %ld\n", sizeof(arr[0]));
+    printf("numb of elem of short arr = %ld\n", sizeof(arr) / sizeof(arr[0]));
+    printf("sizeof(short arr) = %ld\n", sizeof(arr));
+    printf("address of s = %p\n", &s);
+    printf("address of s.i = %p\n", &s.i);
 }
