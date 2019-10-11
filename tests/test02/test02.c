60a57c0e0e0cc7d7749426380f6c0d86c9ddf9f3
butorinab <butorinab@yamoney.ru>
2019-10-11 13:36:30 +0300

    Added an integer division by negative divider in c

--- tests/test02/test02.c
+++ tests/test02/test02.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 // int short arr[20];
-// short arr[10 << 1]
-// short int arr[10 * (10 % -3)];
+// short arr[10 << 1];
+int a = 10, b = -3, c, r;
+
+short int arr[10 * (10 % -3)];
 // arr[40] short;
-short arr[20];
+// short arr[20];
 struct S {
     char c; // 1 Bite
     int i;  // 4 Bite
@@ -20,7 +22,7 @@ struct S ss = {'A', 1};
 
 void main()
 {
-    // s = {'A', 1}; // WORNG
+    // s = {'A', 1}; // WORNG 
 
     s.c = 'B';
     s.i = '2';
@@ -30,4 +32,12 @@ void main()
     printf("sizeof(short arr) = %ld\n", sizeof(arr));
     printf("address of s = %p\n", &s);
     printf("address of s.i = %p\n", &s.i);
+
+    int ch = '%';
+
+    c = a / b;
+    r = a % c;
+
+    printf("c = a / b = %d\n", c);
+    printf("c = a %c b = %d\n", ch, r);
 }
